c1bdad858723c99858180f6831496bc325a83d10
Phil <pronewgen92@gmail.com>
2020-12-19 19:37:36 -0500

    start registerui.

---
+++ client/src/components/UserAuth/AuthContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function authContainer() {
+    return (
+        <div>
+            authcontainer
+        </div>
+    )
+}
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+import AuthContainer from './UserAuth/AuthContainer';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                Hello World
-            </div>
+            <Router>
+                <Switch>
+                    <Route
+                        exact
+                        path='/register'
+                        render={(props) => (
+                            <AuthContainer {...props} />
+                        )}
+                    />
+                </Switch>
+            </Router>
         )
     }
-}
+};
